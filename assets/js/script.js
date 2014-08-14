6bbdaf8ea7d23056ce4c41423a7d0678bcaffb5b
Eli José Carrasquero [PC] <ielijose@gmail.com>
2014-08-14 12:53:50 -0430

    Start

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+var elevator;
+var map;
+var infowindow = new google.maps.InfoWindow();
+var denali = new google.maps.LatLng(10.546991509229592,-71.52147931046784);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setCenter);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function setCenter(pos){
+  alert(pos);
+}
+
+function initialize() {
+  getLocation();
+  var mapOptions = {
+    zoom: 8,
+    center: denali,
+    mapTypeId: 'terrain'
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var myTitle = document.createElement('h3');
+  myTitle.style.color = 'black';
+  myTitle.innerHTML = 'Eli José Carrasquero';
+  var myTextDiv = document.createElement('div');
+  myTextDiv.appendChild(myTitle);
+ // myTextDiv.addClass('menu');
+
+  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(myTextDiv);
+
+  // Create an ElevationService
+  elevator = new google.maps.ElevationService();
+
+  // Add a listener for the click event and call getElevation on that location
+  google.maps.event.addListener(map, 'click', getElevation);
+}
+
+function getElevation(event) {
+
+  var locations = [];
+
+  // Retrieve the clicked location and push it on the array
+  var clickedLocation = event.latLng;
+  locations.push(clickedLocation);
+
+  // Create a LocationElevationRequest object using the array's one value
+  var positionalRequest = {
+    'locations': locations
+  }
+
+  // Initiate the location request
+  elevator.getElevationForLocations(positionalRequest, function(results, status) {
+    if (status == google.maps.ElevationStatus.OK) {
+
+      // Retrieve the first result
+      if (results[0]) {
+
+        var m = results[0].elevation.toFixed(2);
+
+        // Open an info window indicating the elevation at the clicked position
+        infowindow.setContent(results[0].elevation + ' metros.');
+        infowindow.setPosition(clickedLocation);
+        //infowindow.open(map);
+
+        var marker = new google.maps.Marker({
+          position: clickedLocation,
+          map: map,
+          title: m + " m"
+        });
+
+        marker.setMap(map);
+        $(".number").text(m);
+      } else {
+        alert('No results found');
+      }
+    } else {
+     // alert('Elevation service failed due to: ' + status);
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
